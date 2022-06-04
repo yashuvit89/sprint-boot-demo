d4afa42b635aa5e9362c9c7dda4b1413455d2f31
P S L Yaswanth <yashu.vit@gmail.com>
2022-06-04 19:47:41 +0530

    Add bean and path variable

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
    @GetMapping(path = "/hello-world")
    public String helloWorld() {
-       return "hello world";
+       return "hello world ";
+   }
+
+   @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+       return new HelloWorldBean("Hello");
+   }
+
+   @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPath(@PathVariable String name) {
+       return new HelloWorldBean(name);
    }
 }
